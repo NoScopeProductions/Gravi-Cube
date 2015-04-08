3cf01f9bee7743e22bbe9712fd1f70b7c3247c95
Stefano Fiumara <sdf10517@gmail.com>
2015-04-07 20:03:25 -0500

    Initial Project Upload

---
+++ Assets/Scripts/Game/Cube.cs
@@ -0,0 +1,73 @@
+﻿using JetBrains.Annotations;
+using Manager;
+using UnityEngine;
+
+namespace Game
+{
+    [UsedImplicitly]
+    public class Cube : MonoBehaviour
+    {
+        private Renderer _renderer;
+
+        private int _health;
+        private GridManager _gridManager;
+
+        public bool IsEdgeCube { private get; set; }
+
+        [UsedImplicitly]
+        void Start ()
+        {
+            _health = IsEdgeCube ? 1 : 3;
+            _renderer = GetComponent<Renderer>();
+            _gridManager = GameObject.Find("GridManager").GetComponent<GridManager>();
+
+            AdjustCubeColor();
+        }
+
+        public void AddHealth()
+        {
+            Mathf.Clamp(++_health, -1, 3);
+            AdjustCubeColor();
+        }
+
+        private void DecreaseHealth()
+        {
+            Mathf.Clamp(--_health, -1, 3);
+
+            if (_health <= 0)
+            {
+                _gridManager.Grid.Remove(gameObject);
+                Destroy(gameObject);
+            }
+
+            AdjustCubeColor();
+        }
+
+        private void AdjustCubeColor()
+        {
+            switch (_health)
+            {
+                case -1:
+                case  0:
+                case  1:
+                    _renderer.material.color = Color.red;
+                    break;
+                case 2:
+                    _renderer.material.color = Color.yellow;
+                    break;
+                case 3:
+                    _renderer.material.color = Color.white;
+                    break;
+                default:
+                    _renderer.material.color = Color.white;
+                    break;
+            }
+        }
+
+        [UsedImplicitly]
+        public void OnTriggerExit(Collider col)
+        {
+            DecreaseHealth();
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/GridManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Game;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Manager
+{
+    [UsedImplicitly]
+    public class GridManager : MonoBehaviour
+    {
+        [SerializeField, UsedImplicitly] 
+        private GameObject _cubePrefab;
+
+        [SerializeField, UsedImplicitly] 
+        private int _gridWidth;
+
+        [SerializeField, UsedImplicitly]
+        private int _gridHeight;
+
+        public List<GameObject> Grid { get; private set; }
+
+        [UsedImplicitly]
+        void Awake ()
+        {
+            Grid = new List<GameObject>();
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            for (int i = 0; i < _gridWidth; i++)
+            {
+                for (int j = 0; j < _gridHeight; j++)
+                {
+                    CreateCube(i, j);
+                }
+            }
+        }
+
+        private void CreateCube(int i, int j)
+        {
+            var cube = ((GameObject)Instantiate(_cubePrefab, new Vector3(i * 3f, 0f, j * 3f), Quaternion.identity)).GetComponent<Cube>();
+            cube.transform.parent = transform;
+            cube.IsEdgeCube = CreatingEdgeCube(i, j);
+            Grid.Add(cube.gameObject);
+        }
+
+        private bool CreatingEdgeCube(int i, int j)
+        {
+            return i == 0 || j == 0 ||
+                   i == _gridWidth-1 ||
+                   j == _gridHeight-1;
+        }
+    }
+}
