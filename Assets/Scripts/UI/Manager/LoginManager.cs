db69199b116ee6ed3cde430555af9f6b3c08e45d
Stefano Fiumara <sdf10517@gmail.com>
2015-04-07 23:41:41 -0500

    Added Parse and Menu Scene

---
+++ Assets/Scripts/UI/Manager/LoginManager.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+using Parse;
+using System.Threading.Tasks;
+
+namespace UI.Manager
+{
+    [UsedImplicitly]
+    public class LoginManager : MonoBehaviour
+    {
+
+        [SerializeField, UsedImplicitly]
+        private InputField _email;
+
+        [SerializeField, UsedImplicitly]
+        private InputField _password;
+
+        [UsedImplicitly]
+        public void LoginAction()
+        {
+            ParseUser.LogOut();
+
+            ParseUser.LogInAsync(_email.text, _password.text).ContinueWith(t =>
+            {
+                if (t.IsFaulted || t.IsCanceled)
+                {
+                    // The login failed. Check the error to see why.
+
+                    Debug.LogError(t.Exception.Message);
+                }
+                else
+                {
+                    Debug.Log("Login Success");
+                }
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Manager/RegisterManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using JetBrains.Annotations;
+using Parse;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Manager
+{
+    [UsedImplicitly]
+    public class RegisterManager : MonoBehaviour
+    {
+        [SerializeField, UsedImplicitly]
+        private InputField _username;
+
+        [SerializeField, UsedImplicitly]
+        private InputField _email;
+
+        [SerializeField, UsedImplicitly]
+        private InputField _password;
+
+        [SerializeField, UsedImplicitly]
+        private InputField _confirmPassword;
+
+
+        [UsedImplicitly]
+        public void RegisterAction()
+        {
+            StartCoroutine(RegisterCoroutine());
+        }
+
+        private IEnumerator RegisterCoroutine()
+        {
+            if (!_password.text.Equals(_confirmPassword.text))
+            {
+                Debug.LogError("Passwords do not match!");
+                yield return null;
+            }
+
+            var user = new ParseUser
+            {
+                Username = _username.text,
+                Password = _password.text,
+                Email = _email.text
+            };
+
+            var signUpTask = user.SignUpAsync().ContinueWith(task =>
+            {
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    throw task.Exception.Flatten();
+                }
+            });
+
+            while (!signUpTask.IsCompleted) yield return null;
+
+            Debug.Log("Sign Up Successful");
+        }
+    }
+}
+
