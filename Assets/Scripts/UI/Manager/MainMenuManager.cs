b57810e73ef139ece3502f0975bbb8066deb0271
Stefano Fiumara <sdf10517@gmail.com>
2015-04-08 22:30:59 -0500

    Added Main Menu Script

---
+++ Assets/Scripts/UI/Manager/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Parse;
+using UnityEngine;
+
+namespace UI.Manager
+{
+    [UsedImplicitly]
+    public class MainMenuManager : MonoBehaviour
+    {
+
+        [SerializeField, UsedImplicitly]
+        private GameObject _loginPanel;
+
+        [UsedImplicitly]
+        public void LogOutAction()
+        {
+            ParseUser.LogOutAsync();
+
+            _loginPanel.GetComponent<LoginManager>().Show();
+            gameObject.SetActive(false);
+        }
+    }
+}
